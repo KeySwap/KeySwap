2d3d6009fc1b6c1d9f63b215db54e8c092acf11a
Beck L <becklorsch@gmail.com>
2016-01-20 19:54:44 -0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+var config = {
+	username: "TheKeyToSuccessBot",
+	password: "bthommes2017",
+	greetMsg: "Lollery!",
+	admin: '76561197985756607', // Steam ID 64-bit integer
+	domain: "example.com" // Only used to request a new key if you don't have a one already. Update to your own domain.
+};
+
+module.exports = config;
