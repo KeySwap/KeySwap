75df01fda63f0d2e118c8d748c351a6be6672fed
theoneusernameyoullforget <bthommes2017@markdayschool.org>
2016-01-22 07:10:53 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {
 	username: "TheKeyToSuccessBot",
-	password: "bthommes2017",
+	password: "REDACTED",
 	greetMsg: "Lollery!",
 	admin: '76561197985756607', // Steam ID 64-bit integer
 	domain: "example.com" // Only used to request a new key if you don't have a one already. Update to your own domain.
