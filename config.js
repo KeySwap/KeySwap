b41fbe4971985a23b0484cdab6aa96238fa8f565
theoneusernameyoullforget <bthommes2017@markdayschool.org>
2016-01-25 11:41:25 -0800

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-	username: "TheKeyToSuccessBot",
-	password: "wHbfwFyw",
-	greetMsg: "",
-	admin: '76561197985756607', // Steam ID 64-bit integer
-	domain: "example.com" // Only used to request a new key if you don't have a one already. Update to your own domain.
-};
-
-module.exports = config;
